e29f8e1cc668cab16f9215a8f25246afdfb5a087
Flop <robvandermeer@yahoo.com>
2022-08-26 16:25:12 +0200

    updates

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,7 +59,7 @@ const MINT_PRICE = 0.1; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH,
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-08-22T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-08-27T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-08-20T06:05:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
