0e5142bffa5c9cb1c3d6e97c4526f8a7be38fd6b
Flop <robvandermeer@yahoo.com>
2022-08-30 15:55:28 +0200

    gdfjff

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -168,7 +168,7 @@ async function loadInfo() {
 
   let startTime = "";
   if (publicMintActive) {
-    mainHeading.innerText = h1_public_mint_img;
+    image.src = "images/slider/Pi2NFT_com.gif" alt="PiCoin";
     mainHeading.innerText = h1_public_mint;
     mainText.innerText = p_public_mint;
     actionButton.classList.add('hidden');
