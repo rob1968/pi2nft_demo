55dfc50e004bf8eefa90ad74dab3227275b9b9a5
Flop <robvandermeer@yahoo.com>
2022-08-31 14:30:16 +0200

    ddd

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -168,9 +168,7 @@ async function loadInfo() {
 
   let startTime = "";
   if (publicMintActive) {
-
     mainHeading.innerText = h1_public_mint;
-    mainText.innerHTML = h1_public_mint_img;
     mainText.innerText = p_public_mint;
     actionButton.classList.add('hidden');
     mintButton.innerText = button_public_mint;
