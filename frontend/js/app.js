654a575c4ee174b2b1d37fa82196dd75f2ff9e10
Flop <robvandermeer@yahoo.com>
2022-08-31 14:06:18 +0200

    xld

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -170,7 +170,7 @@ async function loadInfo() {
   if (publicMintActive) {
 
     mainHeading.innerText = h1_public_mint;
-    mainHeading.innerHTML = h1_public_mint_img;
+    mainText.innerHTML = h1_public_mint_img;
     mainText.innerText = p_public_mint;
     actionButton.classList.add('hidden');
     mintButton.innerText = button_public_mint;
